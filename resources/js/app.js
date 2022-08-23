33a33d385d461bfcb1b7e4e38a4270246cfd7538
Bryan Hoffman <bryan@spigotdesign.com>
2022-08-23 10:30:39 -0600

    Small changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,146 @@
-!function(e){var n={};function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:o})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(t.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var r in e)t.d(o,r,function(n){return e[n]}.bind(null,r));return o},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="/",t(t.s=0)}({"//zx":function(e,n){},0:function(e,n,t){t("bUC5"),t("ipo3"),t("iY67"),e.exports=t("//zx")},bUC5:function(e,n){var t;document.body.classList.remove("no-js"),(t={navToggle:document.querySelector(".toggle-menu"),body:document.querySelector(".app-header"),search:document.querySelector(".app-search__search-icon"),searchClose:document.querySelector(".app-search__close"),doToggle:function(e){e.preventDefault(),this.body.classList.toggle("nav-open")}}).navToggle.addEventListener("click",function(e){t.doToggle(e)}),function(){for(var e=document.querySelectorAll(".has-children > .menu__link"),n=0;n<e.length;n++)e[n].addEventListener("click",function(){window.innerWidth<900&&(event.preventDefault(),this.parentNode.classList.toggle("subnav-open"))})}(),window.addEventListener("touchstart",function(){for(var e=document.querySelectorAll(".has-children > .menu__link"),n=0;n<e.length;n++)e[n].setAttribute("href","#")}),jQuery(document).ready(function(){jQuery(".menu--shop-by-category .menu__item--ancestor > .menu__link").parent().addClass("subnav-open")}),function(){for(var e=document.querySelectorAll(".menu--shop-by-category .has-children > .menu__link"),n=0;n<e.length;n++)e[n].addEventListener("click",function(){event.preventDefault(),this.parentNode.classList.toggle("subnav-open")})}()},iY67:function(e,n){},ipo3:function(e,n){}});
+/**
+ * Primary front-end script.
+ *
+ * Primary JavaScript file. Any includes or anything imported should
+ * be filtered through this file and eventually saved back into the
+ * `/dist/js/app.js` file.
+ *
+ * @package   LiveLikeSam
+ * @author    Bryan Hoffman <bryan@spigotdesign.com>
+ * @copyright Copyright (c) 2020, Bryan Hoffman
+ * @link      https://spigotdesign.com/
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+
+(function () {
+
+    // Detect for Javascript. Remove .no-js body class added in /app/functions-filters.php
+    var el = document.body;
+    el.classList.remove("no-js");
+
+})();
+
+
+(function () {
+
+    var toggle = {
+        navToggle: document.querySelector('.toggle-menu'),
+        body: document.querySelector('.app-header'),
+        search: document.querySelector('.app-search__search-icon'),
+        searchClose: document.querySelector('.app-search__close'),
+
+
+        doToggle: function (e) {
+            e.preventDefault();
+            this.body.classList.toggle('nav-open');
+        },
+
+    };
+
+    toggle.navToggle.addEventListener('click', function (e) {
+        toggle.doToggle(e);
+    });
+
+}());
+
+(function subnavToggle() {
+    var items = document.querySelectorAll('.menu-item-has-children button');
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', function () {
+            if (window.innerWidth < 900) {
+                event.preventDefault();
+                this.parentNode.classList.toggle('subnav-open');
+            }
+        });
+
+
+    }
+})();
+
+
+
+window.addEventListener('touchstart', function () {
+
+    var items = document.querySelectorAll('.has-children > .menu__link');
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].setAttribute('href', '#');
+    }
+
+});
+
+
+/* Scroll header
+const scrollElement = document.querySelector('body');
+const detached = "detached";
+const invisible = "invisible"
+let previousScroll = 0;
+let menuOffset = 120;
+const detachPoint = 650;
+const hideShowOffset = 20;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  let scrollDifference = Math.abs(currentScroll - previousScroll);
+  // if scrolled past menu
+  if (currentScroll > menuOffset) {
+    // if scrolled past detach point add class to fix menu 
+    if (currentScroll > detachPoint) {
+      scrollElement.classList.add(detached)
+    }
+    // if scrolling faster than hideShowOffset hide/show menu
+    if (scrollDifference >= hideShowOffset) {
+      if (currentScroll > previousScroll) {
+        // Scrolling down, hide menu
+        scrollElement.classList.add(invisible)
+      } else {
+        scrollElement.classList.remove(invisible);
+      }
+    }
+
+  } else {
+    if (currentScroll <= 0) {
+      scrollElement.classList.remove(detached);
+    }
+  }
+  // if user is at the bottom of document show menu
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    scrollElement.classList.remove(invisible);
+  }
+  previousScroll = currentScroll;
+});
+*/
+
+// sticky nav
+
+jQuery(window).scroll(function (e) {
+    var $el = jQuery('.sticky-menu');
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if (jQuery(this).scrollTop() > 200 && !isPositionFixed) {
+        $el.css({
+            'position': 'fixed',
+        });
+
+        $el.addClass('stick-to-top')
+    }
+    if (jQuery(this).scrollTop() < 200 && isPositionFixed) {
+        $el.css({
+            'position': 'static'
+        });
+
+        $el.removeClass('stick-to-top')
+
+    }
+});
+
+// Coverage TABS
+jQuery('.callout-tabs .fl-callout a').click(function (e) {
+    var t = jQuery(this).attr('href');
+    var target = jQuery(t);
+    e.preventDefault();
+    jQuery('.coverage-row.active').removeClass('active');
+    jQuery(t).addClass('active');
+
+});
