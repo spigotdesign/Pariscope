667be24b0e2e78246febdc668d5098a1f4665983
Bryan Hoffman <bryan@spigotdesign.com>
2023-03-20 10:56:20 -0600

    Updated styles to latest
    Based off of Rob Harter website

--- resources/views/searchform.php
+++ resources/views/searchform.php
@@ -1,8 +1,8 @@
 <div class="app-search searchform" role="search">
 
 	<form method="get" class="app-search__search-form" action="<?php echo trailingslashit( home_url() ); ?>">
-		<input class="search-text" type="search" name="s" placeholder="<?php if ( is_search() ) echo esc_attr( get_search_query() ); else esc_attr_e( 'Search this site...', 'pariscope' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
-		<input class="search-submit button" name="submit" type="submit" value="<?php esc_attr_e( '', 'pariscope' ); ?>" />
+		<input class="search-text" type="search" name="s" placeholder="<?php if ( is_search() ) echo esc_attr( get_search_query() ); else esc_attr_e( 'Search this site...', 'rob-harter' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
+		<input class="search-submit button" name="submit" type="submit" value="<?php esc_attr_e( '', 'rob-harter' ); ?>" />
 
 	</form>
 
