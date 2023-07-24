dd4b07cc2fecfc6f885f9f8c78e678045040d685
Bryan Hoffman <bryan@spigotdesign.com>
2023-07-24 13:25:51 -0600

    Migrating to resources/views

--- archive.php
+++ archive.php
@@ -2,9 +2,8 @@
 /**
  * The template for displaying archive pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package Pariscope
+ * 
  */
 
 get_header(); ?>
@@ -13,35 +12,22 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+			<?php get_template_part( 'resources/views/header', 'archive-header' ); ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+			<?php while (have_posts()):the_post(); ?>
+				
+				<?php get_template_part( 'resources/views/content/content', get_post_type() ); ?>
 
-			endwhile;
+			<?php endwhile; ?>
 
-			the_posts_navigation();
+			<?php the_posts_navigation(); ?>
 
-		else :
+		<?php else : ?>
 
-			get_template_part( 'template-parts/content', 'none' );
+			<?php get_template_part( 'resources/views/content/content', 'none' ?>
 
-		endif;
-		?>
+		<?php endif; ?>
 
 	</main>
 
