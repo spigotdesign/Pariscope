b383e0639e02d352c02b778a9830f0efc9dc28f9
Bryan Hoffman <bryan@spigotdesign.com>
2023-07-24 13:26:56 -0600

    Custom Walker nav start

--- app/functions-template.php
+++ app/functions-template.php
@@ -28,3 +28,43 @@
     return $classes;
 
 }, 0 );
+
+/**
+ * Custom Navigation Walker
+ */
+
+class Pariscope_Walker extends Walker_Nav_Menu {
+  
+  function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
+    $object = $item->object;
+    $type = $item->type;
+    $title = $item->title;
+    $permalink = $item->url;
+    $classes = $item->classes;
+
+    $output .= "<li id='menu-item-". $item->ID ."' class='menu__item " .  implode(" ", $classes) . "'>";
+    
+    if( $permalink && $permalink != '#' ) {
+      $output .= '<a class="menu__link" href="' . $permalink . '">';
+      
+    } else {
+        $output .= '';
+      }
+    
+    if($args->walker->has_children) {
+        $output .= '<button type="button" class="dropdown__title" aria-expanded="false" aria-controls="nav-dropdown-'. $item->ID .'">' ;
+    }
+     
+    $output .= $item->title;
+    
+    if($args->walker->has_children) {
+        $output .= '</button>' ;
+    }
+
+    if( $permalink && $permalink != '#' ) {
+      $output .= '</a>';
+    } else {
+      $output .= '';
+    }
+  }
+}
--- resources/views/nav/menu-primary.php
+++ resources/views/nav/menu-primary.php
@@ -1,10 +1,19 @@
-cha cha checkdate<?php 
+<?php if ( has_nav_menu( 'primary' ) ) { ?>
 
-wp_nav_menu( [
+	<nav class="menu menu--primary">
+
+		<h3 class="menu__title screen-reader-text">Primary Menu</h3>
+	
+		<?php wp_nav_menu( [
 			'theme_location' => 'primary',
 			'container'      => '',
 			'menu_id'        => '',
 			'menu_class'     => 'menu__items',
 			'items_wrap'     => '<ul class="%2$s">%3$s</ul>',
-			'item_spacing'   => 'discard'
+			'item_spacing'   => 'discard',
+			'walker'		 => new Pariscope_Walker()
 		] ) ?>
+
+	</nav>
+
+<?php } ?>
