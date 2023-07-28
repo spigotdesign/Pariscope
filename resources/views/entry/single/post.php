debdd7ca3bfdb1f5a46ffff67db54e00f1ef11f3
Bryan Hoffman <bryan@spigotdesign.com>
2023-07-28 13:04:33 -0600

    Comments migrated to /views

--- resources/views/entry/single/post.php
+++ resources/views/entry/single/post.php
@@ -1,7 +1,13 @@
-<article class="">
+<article class=""> 
 	
 	<div class="entry__content">
 		<?php the_content() ?>
 	</div>
 
 </article>
+
+<?php if ( comments_open() ) : ?>
+				
+	<?php comments_template('/resources/views/comments/comments.php'); ?>
+
+<?php endif; ?>
